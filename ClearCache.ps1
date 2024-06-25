9bcacc60efb21658733d04978ac64970e99ca1b7
GNUnix <105981834+Cerberos4238@users.noreply.github.com>
2024-06-25 15:40:24 +0200

    Update ClearCache.ps1

--- ClearCache.ps1
+++ ClearCache.ps1
@@ -2,20 +2,21 @@ $cache = "$env:appdata\Microsoft\Windows\PowerShell\PSReadline\*"
 
 if (Test-Path $cache) {
     Remove-Item $cache -Force
-    Write-Host "L'historique des commandes a été effacé" -ForegroundColor Red
+    Write-Host "Commands history cleared" -ForegroundColor Red
 }else {
-    Write-Host "L'historique des commandes est déjà vide" -ForegroundColor Green
+    Write-Host "Commands history already emptied" -ForegroundColor Green
 }
 
 Set-Clipboard -Value ""
+Write-Host "The clipboard has been deleted" -ForegroundColor Green
 Clear-History
 
 $tempFiles = "C:\temp\*"
 
 if (Test-Path $tempFiles) {
     Remove-Item $tempFiles -Force
-    Write-Host "Le dossier temp a été vidé" -ForegroundColor Red
+    Write-Host "The temp file has been emptied" -ForegroundColor Red
 
 }else {
-    Write-Host "Le dossier temp est déjà vide" -ForegroundColor Green
+    Write-Host "The temp folder is already empty" -ForegroundColor Green
 }
